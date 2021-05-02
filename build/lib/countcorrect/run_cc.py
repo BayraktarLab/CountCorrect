322445ac69bc2a98b5a2267a7a3774ac86a30c32
Alexander Aivazidis <aa16@sanger.ac.uk>
2021-05-02 21:59:13 +0100

    added naive correction

--- build/lib/countcorrect/run_cc.py
+++ build/lib/countcorrect/run_cc.py
@@ -8,49 +8,57 @@ from countcorrect.ProbeCounts__GeneralModel import ProbeCounts_GeneralModel
 import numpy as np
 
 def run_countcorrect(counts_geneProbes, counts_negativeProbes, counts_nuclei,
-                     n_factors = 10,
+                     n_factors = 30,
                      total_iterations = 20000,
                      learning_rate = 0.01,
                      posterior_samples = 100,
-                     verbose = True):
+                     verbose = True,
+                     naive = False):
     
-    # Check input is sensible:
+    if not naive:
     
-    if np.any(counts_nuclei == 0) or np.any(np.isnan(counts_nuclei)) or np.any(np.isinf(counts_nuclei)):
-        raise ValueError('Some of your nuclei counts are 0, nan or inf')
-    
-    if verbose:
-        
-        print('Initializing model...')
-    
-    model = ProbeCounts_GeneralModel(
-        X_data = counts_geneProbes,
-        Y_data = counts_negativeProbes,
-        nuclei = counts_nuclei,
-        n_factors = n_factors)
-    
-    if verbose:
-        
-        print('Fitting model ...')
-    
-    model.fit_advi_iterative(n_iter = total_iterations, learning_rate = learning_rate, n=1, method='advi')
-    
-    if verbose:
+        if np.any(counts_nuclei == 0) or np.any(np.isnan(counts_nuclei)) or np.any(np.isinf(counts_nuclei)):
+            raise ValueError('Some of your nuclei counts are 0, nan or inf')
 
-        model.plot_history()
-        plt.show()
-        model.plot_history(iter_start = int(np.round(total_iterations - (total_iterations*0.1))),
-                           iter_end = int(total_iterations))
-        plt.show()
+        if verbose:
+
+            print('Initializing model...')
+
+        model = ProbeCounts_GeneralModel(
+            X_data = counts_geneProbes,
+            Y_data = counts_negativeProbes,
+            nuclei = counts_nuclei,
+            n_factors = n_factors)
+
+        if verbose:
+
+            print('Fitting model ...')
+
+        model.fit_advi_iterative(n_iter = total_iterations, learning_rate = learning_rate, n=1, method='advi')
+
+        if verbose:
+
+            model.plot_history()
+            plt.show()
+            model.plot_history(iter_start = int(np.round(total_iterations - (total_iterations*0.1))),
+                               iter_end = int(total_iterations))
+            plt.show()
+
+            print('Sampling from posterior distribution...')
+
+        model.sample_posterior(node='all', n_samples=posterior_samples, save_samples=False);
+
+        model.compute_X_corrected()          
+
+        if verbose:
+
+            print('Done.')
+            
+        return model.X_corrected_mean
+            
+    else:
         
-        print('Sampling from posterior distribution...')
-    
-    model.sample_posterior(node='all', n_samples=posterior_samples, save_samples=False);
-    
-    model.compute_X_corrected()          
-    
-    if verbose:
+        X_naive = np.round(np.clip(counts_geneProbes - np.mean(counts_negativeProbes, axis = 1).reshape(np.shape(counts_negativeProbes)[0],1), a_min = 0, a_max = None))
+        
+        return X_naive
               
-        print('Done.')
-              
-    return model.X_corrected_mean
